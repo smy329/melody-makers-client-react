290834cf36145e2d9a6291975e2d61d2d73bfc1f
Syed Mohammad Yasir <smy329@gmail.com>
2023-06-07 01:52:28 +0600

    project initialized

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/images/logo.png';
+
+const Main = () => {
+  return (
+    <div>
+      <h1>Welcome TO melody-makers-camp-client</h1>
+      <img src={logo} alt="" className="text-red-800 " />
+    </div>
+  );
+};
+
+export default Main;
